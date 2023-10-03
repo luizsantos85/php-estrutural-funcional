8a9107aae65256b63435e8cb28924fa503379fe1
Luiz Henrique Santos <luiz.santos85@gmail.com>
2023-10-02 22:12:20 -0300

    alterando funcoes database

--- app/database/querys.php
+++ app/database/querys.php
@@ -32,3 +32,26 @@ function findBy($table,$field, $value, $fields = '*')
         echo '</pre>';
     }
 }
+
+function create(string $table, array $data)
+{
+    try {
+        if (!isArrayAssociative($data)) {
+            throw new Exception("O array tem que ser associativo.", 1);
+        }
+
+        $pdo = connectDb();
+
+        $sql = "insert into {$table}(";
+        $sql .= implode(',', array_keys($data)) . ") values(";
+        $sql .= ':' . implode(',:', array_keys($data)) . ")";
+
+        $insert = $pdo->prepare($sql);
+        return $insert->execute($data);
+    } catch (PDOException $e) {
+        echo '<pre>';
+        print_r($e->getMessage());
+        echo '</pre>';
+        exit;
+    }
+}
